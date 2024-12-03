9d1a31a742bb09b81ee061ec21c305b6cab496fc
Lincoln Hoey Moore <johlinco@gmail.com>
2024-12-02 20:44:06 -0500

    day one part 2

--- day-one/day-one.py
+++ day-one/day-one.py
@@ -1,8 +1,8 @@
-example_rows = open("example.txt", "r")
-input_rows = open("input.txt", "r")
+example_rows = open("example.txt")
+input_rows = open("input.txt")
 
 
-def part1(rows):
+def read_input(rows):
     left_col, right_col = [], []
     for line in rows.readlines():
         nums = line.split(" ")
@@ -11,6 +11,12 @@ def part1(rows):
 
     left_col.sort()
     right_col.sort()
+
+    return left_col, right_col
+
+
+def part1(rows):
+    left_col, right_col = read_input(rows)
     diff_sum = 0
 
     for i in range(0, len(right_col)):
@@ -19,8 +25,27 @@ def part1(rows):
     return diff_sum
 
 
-print(part1(example_rows))
-print(part1(input_rows))
+def part2(rows):
+    left_col, right_col = read_input(rows)
+    similarity_score = 0
+    freq_table = {}
 
+    for num in right_col:
+        if num in freq_table:
+            freq_table[num] += 1
+        else:
+            freq_table[num] = 1
+
+    for num in left_col:
+        if num in freq_table:
+            similarity_score += num * freq_table[num]
+
+    return similarity_score
+
+
+# print(part1(example_rows))
+# print(part1(input_rows))
+print(part2(example_rows))
+print(part2(input_rows))
 example_rows.close()
 input_rows.close()
