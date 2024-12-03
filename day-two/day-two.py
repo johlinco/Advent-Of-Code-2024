7b24613985faa3d95d91867362008df0f04d96a4
Lincoln Hoey Moore <johlinco@gmail.com>
2024-12-02 21:19:02 -0500

    part one

--- day-two/day-two.py
+++ day-two/day-two.py
@@ -2,5 +2,36 @@ example_rows = open("example.txt")
 input_rows = open("input.txt")
 
 
+def safe_or_unsafe(arr):
+    if int(arr[1]) > int(arr[0]):
+        dir = "up"
+    else:
+        dir = "not up"
+
+    for i in range(1, len(arr)):
+        first = int(arr[i-1])
+        second = int(arr[i])
+        if second == first or abs(second - first) > 3:
+            return False
+        if dir == "up" and second < first:
+            return False
+        if dir != "up" and second > first:
+            return False
+
+    return True
+
+
+def part1(rows):
+    safe_reports = 0
+    for line in rows.readlines():
+        report = line.split(" ")
+        if (safe_or_unsafe(report)):
+            safe_reports += 1
+
+    return safe_reports
+
+
+print(part1(example_rows))
+print(part1(input_rows))
 example_rows.close()
 input_rows.close()
